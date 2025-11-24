aeb24d8e162d20f6bffab79b3cdb793c81d3c117
mnenie <1alexpeshkov@gmail.com>
2025-11-24 21:02:56 +0300

    feat: add eslint and husky

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    type: 'lib',
+    stylistic: true,
+    typescript: true,
+  },
+  {
+    files: ['**/core/**/*.ts'],
+    rules: {
+      'no-console': 'error',
+      'no-alert': 'error',
+      'ts/explicit-function-return-type': 'off',
+      'antfu/top-level-function': 'off',
+    },
+  },
+  {
+    ignores: [
+      'tests/**/fixtures/**/*.{js,ts}',
+      '**/coverage/**',
+    ],
+  },
+)
